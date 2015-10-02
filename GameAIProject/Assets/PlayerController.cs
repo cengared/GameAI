a759d0a26a61289e2cbc041811f8d96d9744c9dd
Daniel Stokoe <ma301ds@gold.ac.uk>
2015-10-02 22:33:53 +0100

    For scene1 work from 01/10/15

---
+++ GameAIProject/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		speed = 1.0;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveH, moveV;
+		if (Input.GetKey (KeyCode.W)
+		    moveV = 1.0;
+		if (Input.GetKey (KeyCode.D)
+		    moveH = 1.0;
+		
+		
+		Vector3 movement = new Vector3 (moveH, 0.0f, moveV);
+		
+		rb.AddForce (movement * speed);
+	}
+}
